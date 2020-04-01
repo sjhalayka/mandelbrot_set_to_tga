34a5780148f469457a4cfb1c35cdeb6abb5779f0
Shawn Halayka <sjhalayka@gmail.com>
2020-04-01 10:54:17 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@ int main(int argc, char **argv)
 {
     const size_t res = 3000;
 
-    const float x_grid_max = 2;
-    const float x_grid_min = -x_grid_max;
+    const float x_grid_max = 1.5;
+    const float x_grid_min = -2.5;
     const size_t x_res = res;
     const complex<float> x_step_size((x_grid_max - x_grid_min) / (x_res - 1), 0);
 
     const float y_grid_max = 2;
-    const float y_grid_min = -y_grid_max;
+    const float y_grid_min = -2;
     const size_t y_res = res;
     const complex<float> y_step_size(0, (y_grid_max - y_grid_min) / (y_res - 1));
 
